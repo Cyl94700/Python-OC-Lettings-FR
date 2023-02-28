d7d3ddfd9832ec01e0cbf60ed21b04fce7ef142e
Cyl94700 <cyrilcouybes@gmail.com>
2023-02-28 23:49:41 +0100

    insert lettings data from oc_lettings_site

--- oc_lettings_site/lettings/views.py
+++ oc_lettings_site/lettings/views.py
@@ -7,7 +7,7 @@ from .models import Letting
 # faucibus orci luctus et ultrices posuere cubilia curae; Cras eget scelerisque.
 
 
-def lettings_index(request):
+def index(request):
     lettings_list = Letting.objects.all()
     context = {'lettings_list': lettings_list}
     return render(request, 'index.html', context)
