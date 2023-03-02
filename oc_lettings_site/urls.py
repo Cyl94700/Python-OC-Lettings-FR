ddd748dcbe589211901930befa23368baa6c5ed9
Cyl94700 <cyrilcouybes@gmail.com>
2023-03-02 10:59:45 +0100

    update urls, views and templates for profiles app

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('', include('oc_lettings_site.lettings.urls')),
+    path('', include('oc_lettings_site.profiles.urls')),
     path('admin/', admin.site.urls),
 ]
