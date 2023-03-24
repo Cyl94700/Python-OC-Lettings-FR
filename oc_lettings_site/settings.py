701bbfb9e0c7fb156a4528996aa26a61726a842f
Cyl94700 <cyrilcouybes@gmail.com>
2023-03-25 00:01:15 +0100

    update settings config

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,11 +1,11 @@
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
