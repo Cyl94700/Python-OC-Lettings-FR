f599bb20613cd698197d63554313ad1f0a0e7e30
Cyl94700 <cyrilcouybes@gmail.com>
2023-03-24 23:24:13 +0100

    update settings config

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -14,6 +14,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if os.path.isfile(os.path.join(BASE_DIR, '.env')):
     load_dotenv()
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
