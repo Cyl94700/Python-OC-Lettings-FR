31825b5523d109d55e1eb7044cb22b0bac01f8d7
Cyl94700 <cyrilcouybes@gmail.com>
2023-03-25 08:58:13 +0100

    update settings config

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -11,8 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # Take environment variables from .env file
-if os.path.isfile(os.path.join(BASE_DIR, '.env')):
-    load_dotenv()
+# if os.path.isfile(os.path.join(BASE_DIR, '.env')):
+#    load_dotenv()
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
